16b9e45fdb3488dcbb4fb01504fd0b344d41eef7
baduga <baduga@localhost>
2018-01-06 02:01:13 -0200

    gitignore

--- src/main/java/engine/controllers/RecommenderController.java
+++ src/main/java/engine/controllers/RecommenderController.java
@@ -1,9 +1,5 @@
 package engine.controllers;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import engine.entities.Similarity;
 import engine.services.RecommenderService;
 import message.Message;
 
